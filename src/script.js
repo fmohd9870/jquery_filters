c454e3e771b601ceed1f4631c91c62ab14f58325
Mohd <mohdfaiz1848816@gmail.com>
2022-02-10 16:14:21 +0530

    advance filters and search key up functionality

--- src/script.js
+++ src/script.js
@@ -39,44 +39,81 @@ var products = [
 $(document).ready(function () {
   display_products();
   $("#brand").on("change", function () {
-    temp = 0;
+    table = 0;
     if ($("#brand").val() == "1") {
       display_products();
     } else {
-      var values = $("#brand").val();
-      by_brand(temp, values);
+      var arg1 = $("#brand").val();
+      by_brand(table, arg1);
     }
   });
+  $("#brand").on("change", function () {
+    if (
+      $("#os").on("change", function () {
+        table = 0;
+        var arg1 = $("#brand").val();
+        var arg2 = $("#os").val();
+        if (arg2 == 1) {
+          by_brand(table, arg1);
+        } else {
+          run1(table, arg1, arg2);
+        }
+        if (arg1 == 1 && arg2 == 1) {
+          console.log("all brand all os");
+          display_products();
+        }
+      })
+    );
+  });
+
   $("#os").on("change", function () {
-    temp = 0;
+    table = 0;
     if ($("#os").val() == "1") {
       display_products();
     } else {
-      var values = $("#os").val();
-      by_os(temp, values);
+      var arg1 = $("#os").val();
+      by_os(table, arg1);
     }
   });
-  $("#search-btn").click(function () {
-    var temp = 0;
-    if($("#search-input").val()==""){
+  $("#os").on("change", function () {
+    if (
+      $("#brand").on("change", function () {
+        table = 0;
+        var arg1 = $("#brand").val();
+        var arg2 = $("#os").val();
+        if (arg1 == 1) {
+          by_os(table, arg2);
+        } else {
+          run1(table, arg1, arg2);
+        }
+        if (arg2 == 1 && arg1 == 1) {
+          console.log("all brand all os");
+          display_products();
+        }
+      })
+    );
+  });
+  $("#search-input").keyup(function () {
+    var table = 0;
+    if ($("#search-input").val() == "") {
       display_products();
-    }else{
-    var values = $("#search-input").val();
-    by_search(temp, values);
+    } else {
+      var arg1 = $("#search-input").val();
+      by_search(table, arg1);
     }
   });
   $("#tbody").on("click", ".delete", function () {
     console.log("hii");
-    var values = $(this).attr("data-id");
+    var arg1 = $(this).attr("data-id");
     console.log("delete id=" + $(this).attr("data-id"));
-    $("#r" + values).css("display", "none");
+    $("#r" + arg1).css("display", "none");
     // var row_id=$(this).attr("data-id")
   });
 });
 function display_products() {
-  var temp = 0;
+  var table = 0;
   for (var i = 0; i < products.length; i++) {
-    temp +=
+    table +=
       "<tr class='row' id='r" +
       products[i].id +
       "'><td>" +
@@ -91,15 +128,15 @@ function display_products() {
       products[i].id +
       "' class='delete' value='X'></td></tr>";
   }
-  $("#tbody").html(temp);
+  $("#tbody").html(table);
 }
-function by_brand(temp, values) {
+function by_brand(table, arg1) {
   console.log("brand selected");
-  console.log(values);
+  console.log(arg1);
   for (var i = 0; i < products.length; i++) {
-    if ("'" + values + "'" == "'" + products[i].brand + "'") {
+    if ("'" + arg1 + "'" == "'" + products[i].brand + "'") {
       console.log(products[i].brand);
-      temp +=
+      table +=
         "<tr class='row' id='r" +
         products[i].id +
         "'><td>" +
@@ -115,15 +152,15 @@ function by_brand(temp, values) {
         "' class='delete' value='X'></td></tr>";
     }
   }
-  $("#tbody").html(temp);
+  $("#tbody").html(table);
 }
-function by_os(temp, values) {
+function by_os(table, arg1) {
   console.log("OS selected");
-  console.log(values);
+  console.log(arg1);
   for (var i = 0; i < products.length; i++) {
-    if ("'" + values + "'" == "'" + products[i].os + "'") {
+    if ("'" + arg1 + "'" == "'" + products[i].os + "'") {
       console.log(products[i].brand);
-      temp +=
+      table +=
         "<tr class='row' id='r" +
         products[i].id +
         "'><td>" +
@@ -139,15 +176,20 @@ function by_os(temp, values) {
         "' class='delete' value='X'></td></tr>";
     }
   }
-  $("#tbody").html(temp);
+  $("#tbody").html(table);
 }
-function by_search(temp, values) {
+function by_search(table, arg1) {
   console.log("brand selected");
-  console.log(values);
+  console.log(arg1);
   for (var i = 0; i < products.length; i++) {
-    if ("'" + values + "'" == "'" + products[i].id + "'" || "'" + values + "'" == "'" + products[i].name + "'" || "'" + values + "'" == "'" + products[i].brand + "'" || "'" + values + "'" == "'" + products[i].os + "'"  ) {
-      console.log(products[i].brand);
-      temp +=
+    if (
+      products[i].id.match(arg1) ||
+      products[i].name.toUpperCase().match(arg1.toUpperCase()) ||
+      products[i].brand.toUpperCase().match(arg1.toUpperCase()) ||
+      products[i].os.toUpperCase().match(arg1.toUpperCase())
+    ) {
+      console.log(products[i].name.match(arg1));
+      table +=
         "<tr class='row' id='r" +
         products[i].id +
         "'><td>" +
@@ -163,5 +205,28 @@ function by_search(temp, values) {
         "' class='delete' value='X'></td></tr>";
     }
   }
-  $("#tbody").html(temp);
+  $("#tbody").html(table);
+}
+function run1(table, arg1, arg2) {
+  console.log("brand and OS selected");
+  console.log(arg1);
+  for (var i = 0; i < products.length; i++) {
+    if (products[i].brand == arg1 && products[i].os == arg2) {
+      table +=
+        "<tr class='row' id='r" +
+        products[i].id +
+        "'><td>" +
+        products[i].id +
+        "</td><td>" +
+        products[i].name +
+        "</td><td>" +
+        products[i].brand +
+        "</td><td>" +
+        products[i].os +
+        "</td><td><input type='button' data-id='" +
+        products[i].id +
+        "' class='delete' value='X'></td></tr>";
+    }
+  }
+  $("#tbody").html(table);
 }
